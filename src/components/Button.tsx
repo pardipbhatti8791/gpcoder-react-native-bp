1c1de2465b6ea8bdb780ce43822a0fc748d2ae11
Pardip Bhatti <sandeepk9187@gmail.com>
2021-12-20 13:48:46 +0530

    working on delte

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
-import {withTheme} from 'styled-components';
+import { TouchableOpacity } from 'react-native';
+import { withTheme } from 'styled-components';
 // @ts-ignore
 import styled from 'styled-components/native';
 
@@ -9,20 +9,22 @@ type PrimaryButtonProps = {
     btnText: string;
     loading?: boolean;
     backgroundColor?: string;
-    heightBT?:number;
+    heightBT?: number;
 };
 
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({
-                                                         onPress,
-                                                         btnText,
-                                                         loading = false,
-                                                         backgroundColor,
-                                                            heightBT,
-                                                     }) => {
+    onPress,
+    btnText,
+    loading = false,
+    backgroundColor,
+    heightBT,
+}) => {
     return (
         <TouchableOpacity onPress={() => onPress()}>
-            <PrimaryButton__Wrapper backgroundColor={backgroundColor} height={heightBT}>
-                <PrimaryButton__Wrapper__Text >
+            <PrimaryButton__Wrapper
+                backgroundColor={backgroundColor}
+                height={heightBT}>
+                <PrimaryButton__Wrapper__Text>
                     {loading ? 'Loading...' : btnText}
                 </PrimaryButton__Wrapper__Text>
             </PrimaryButton__Wrapper>
@@ -34,14 +36,15 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({
 export default withTheme(PrimaryButton);
 
 const PrimaryButton__Wrapper = styled.View`
-  justify-content: center;
-  align-items: center;
-  background-color: ${({theme, backgroundColor}: any) =>
-          backgroundColor ? backgroundColor : theme.colors.greenColor};
-  height: ${({heightBt} :any) => heightBt ? heightBt : 55}px;
-  border-radius: 8px;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme, backgroundColor }: any) =>
+        backgroundColor ? backgroundColor : theme.colors.greenColor};
+    height: ${({ height }: any) => (height ? height : 55)}px;
+    border-radius: 8px;
+
 `;
 const PrimaryButton__Wrapper__Text = styled.Text`
-  color: ${({theme}: any) => theme.colors.text};
-  padding: 10px;
+    color: ${({ theme }: any) => theme.colors.text};
+    padding: 10px;
 `;
